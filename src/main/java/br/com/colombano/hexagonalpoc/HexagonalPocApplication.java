d4e2e7d4d9698e9d420df617d7f4a89af9b5332b
Pedro Colombano <pedromaiacolombano@gmail.com>
2025-09-18 23:40:45 -0300

    feat: project created

---
+++ src/main/java/br/com/colombano/hexagonalpoc/HexagonalPocApplication.java
@@ -0,0 +1,13 @@
+package br.com.colombano.hexagonalpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalPocApplication.class, args);
+    }
+
+}
